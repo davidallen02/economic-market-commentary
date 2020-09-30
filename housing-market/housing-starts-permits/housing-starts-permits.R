e2169539e4a7a8eab83b5e2359bd1d778a1747c1
David Allen <davidallen02@me.com>
2020-09-30 11:05:59 -0700

    add housing starts page

---
+++ housing-market/housing-starts-permits/housing-starts-permits.R
@@ -0,0 +1,41 @@
+library(magrittr)
+
+lay <- pamngr::set_layout(11)
+
+title <- pamngr::set_title("Housing Starts")
+
+path <- if(Sys.info()["nodename"] %in% c("BBJW", "BBDA")){
+  "R:/David/"
+} else {
+  "~/OneDrive/PAMGMT"
+}
+
+load(file = paste0(path, "/Economics/data/housing-starts/output/tam/housing-starts.RData"))
+housing_starts <- p + ggplot2::theme(legend.position = "none", 
+                                          plot.caption    = ggplot2::element_blank())
+
+load(file = paste0(path, "/Economics/data/housing-starts/output/tam/housing-starts-northeast.RData"))
+northeast <- p + ggplot2::theme(plot.caption = ggplot2::element_blank())
+
+load(file = paste0(path, "/Economics/data/housing-starts/output/tam/housing-starts-midwest.RData"))
+midwest <- p + ggplot2::theme(plot.caption = ggplot2::element_blank())
+
+load(file = paste0(path, "/Economics/data/housing-starts/output/tam/housing-starts-south.RData"))
+south <- p + ggplot2::theme(plot.caption = ggplot2::element_blank())
+
+load(file = paste0(path, "/Economics/data/housing-starts/output/tam/housing-starts-west.RData"))
+west <- p + ggplot2::theme(plot.caption = ggplot2::element_blank())
+
+foo <- gridExtra::grid.arrange(grobs = list(title, 
+                                            housing_starts,
+                                            northeast,
+                                            midwest,
+                                            south,
+                                            west), 
+                               layout_matrix = lay)
+
+ggplot2::ggsave("./housing-market/housing-starts-permits/housing-starts-permits.png", 
+                plot = foo, 
+                width = 10, 
+                height = 6.75, 
+                units = "in")
