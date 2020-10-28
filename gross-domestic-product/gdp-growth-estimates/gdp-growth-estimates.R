6bbdefe0788b533bfc5aea51aa0b6b7d71c905cd
David Allen <davidallen02@me.com>
2020-10-28 11:49:08 -0700

    new gdp chart for JW

--- gross-domestic-product/gdp-growth-estimates/gdp-growth-estimates.R
+++ gross-domestic-product/gdp-growth-estimates/gdp-growth-estimates.R
@@ -1,43 +1,22 @@
 library(magrittr)
 
-lay <- readxl::read_excel("layouts.xlsx", 
-                          sheet     = "lay-9", 
-                          range     = "A1:AN28", 
-                          col_names = TRUE) %>% 
-  as.matrix()
+lay <- pamngr::set_layout(19)
 
-title <- grid::textGrob("Gross Domestic Product",
-                        x    = 0, 
-                        y    = 0.5,
-                        just = "left",
-                        gp   = grid::gpar(
-                          fontface = "bold",
-                          fontsize = 36,
-                          col      = "#850237"))
+title <- pamngr::set_title("Gross Domestic Product")
 
-load("~/OneDrive/PAMGMT/Economics/data/gross-domestic-product/output/tam/consumption-growth-with-estimates.RData")
-legend <- p %>% pamngr::get_legend()
-consumption_growth_with_estimates <- p + ggplot2::theme(legend.position = "none")
+p1 <- pamngr::run_and_load("gross-domestic-product", "gdp-growth-with-estimates")
+p1 <- p1 + ggplot2::theme(legend.position = "none")
 
-load("~/OneDrive/PAMGMT/Economics/data/gross-domestic-product/output/tam/private-invest-growth-with-estimates.RData")
-private_investment_growth_with_estimates <- p + ggplot2::theme(legend.position = "none")
 
-load("~/OneDrive/PAMGMT/Economics/data/gross-domestic-product/output/tam/government-spending-growth-with-estimates.RData")
-government_spending_growth_with_estimates <- p + ggplot2::theme(legend.position = "none")
+p2 <- pamngr::run_and_load("gross-domestic-product", "gdp-growth-estimates")
+p2 <- p2 + 
+  ggplot2::theme(legend.position = "none", plot.caption = ggplot2::element_blank()) 
+  
 
-load("~/OneDrive/PAMGMT/Economics/data/gross-domestic-product/output/tam/import-growth-with-estimates.RData")
-import_growth_with_estimates <- p + ggplot2::theme(legend.position = "none")
+legend <- p1 %>% pamngr::get_legend()
 
-load("~/OneDrive/PAMGMT/Economics/data/gross-domestic-product/output/tam/export-growth-with-estimates.RData")
-export_growth_with_estimates <- p + ggplot2::theme(legend.position = "none")
 
-foo <- gridExtra::grid.arrange(grobs = list(title, 
-                                            consumption_growth_with_estimates, 
-                                            private_investment_growth_with_estimates,
-                                            government_spending_growth_with_estimates,
-                                            import_growth_with_estimates,
-                                            export_growth_with_estimates,
-                                            legend), 
-                               layout_matrix = lay)
+
+foo <- gridExtra::grid.arrange(grobs = list(title, p1, p2, legend), layout_matrix = lay)
 
 ggplot2::ggsave("gross-domestic-product/gdp-growth-estimates/gdp-growth-estimates.png", plot = foo, width = 10, height = 6.75, units = "in")
