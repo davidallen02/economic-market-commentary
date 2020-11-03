4c4888e7af130a62d8e4c0ab3a6cea39c00bbc08
David Allen <davidallen02@me.com>
2020-11-03 10:37:42 -0800

    utilize new pamngr functions

--- gross-domestic-product/gdp-composition/gdp-composition.R
+++ gross-domestic-product/gdp-composition/gdp-composition.R
@@ -1,33 +1,21 @@
 library(magrittr)
 
-lay <- readxl::read_excel("layouts.xlsx", 
-                          sheet     = "lay-8", 
-                          range     = "A1:AN28", 
-                          col_names = TRUE) %>% 
-  as.matrix()
+lay <- pamngr::set_layout(8)
 
-title <- grid::textGrob("Gross Domestic Product",
-                        x    = 0, 
-                        y    = 0.5,
-                        just = "left",
-                        gp   = grid::gpar(
-                          fontface = "bold",
-                          fontsize = 36,
-                          col      = "#850237"))
+title <- pamngr::set_title("Gross Domestic Product")
 
-load("~/OneDrive/PAMGMT/Economics/data/gross-domestic-product/output/tam/gdp-composition.RData")
-gdp_composition <- p 
+gdp_composition <- pamngr::run_and_load("gross-domestic-product", "gdp-composition")
 
-load("~/OneDrive/PAMGMT/Economics/data/gross-domestic-product/output/tam/personal-consumption-composition.RData")
-personal_consumption_composition <- p %>% 
+personal_consumption_composition <- pamngr::run_and_load(
+  "gross-domestic-product", "personal-consumption-composition") %>% 
   pamngr::pam_plot(plot_title = "Personal Consumption", caption = FALSE)
 
-load("~/OneDrive/PAMGMT/Economics/data/gross-domestic-product/output/tam/fixed-investment-composition.RData")
-fixed_investment_composition <- p %>% 
+fixed_investment_composition <- pamngr::run_and_load("gross-domestic-product",
+                                                     "fixed-investment-composition") %>% 
   pamngr::pam_plot(plot_title = "Fixed Investment", caption = FALSE)
 
-load("~/OneDrive/PAMGMT/Economics/data/gross-domestic-product/output/tam/government-spending-composition.RData")
-government_spending_composition <- p %>% 
+government_spending_composition <- pamngr::run_and_load("gross-domestic-product",
+                                                        "government-spending-composition") %>% 
   pamngr::pam_plot(plot_title = "Government Spending", caption = FALSE) 
 
 foo <- gridExtra::grid.arrange(grobs = list(title, 
@@ -37,5 +25,6 @@ foo <- gridExtra::grid.arrange(grobs = list(title,
                                             government_spending_composition), 
                                layout_matrix = lay)
 
-ggplot2::ggsave("gross-domestic-product/gdp-composition/gdp-composition.png", plot = foo, width = 10, height = 6.75, units = "in")
+ggplot2::ggsave("gross-domestic-product/gdp-composition/gdp-composition.png", 
+                plot = foo, width = 10, height = 6.75, units = "in")
 
